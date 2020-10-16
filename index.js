70ea3c109f4c57d18dec4db5b8b00fbdba707a53
Nikita Johnson <nikita_johnson@hotmail.com>
2020-10-16 22:38:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ console.log(string.toLowerCase())
 }
 
 function sayHiToGrandma(string){
-if(string.toLowerCase()){
+if(whisper()){
   return "I can't hear you!"
 } else if(string.toUpperCase(string)){
   return "YES INDEED!"
